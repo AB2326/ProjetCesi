e708a4c482995eab82017068fec584e25d8eac93
AB2326 <adam.boussatha@viacesi.fr>
2024-02-07 14:20:27 +0100

    base du projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/user', name: 'app_user')]
-class UserController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('', name: 'app_user_index')]
+    #[Route('', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
