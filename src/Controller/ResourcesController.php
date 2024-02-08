ce9c207644adfc4291db242da1ef2845a84ef9d9
AB2326 <adam.boussatha@viacesi.fr>
2024-02-08 09:00:42 +0100

    Création de routes + html formulaire

--- src/Controller/ResourcesController.php
+++ src/Controller/ResourcesController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ResourcesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/resources', name: 'app_resources')]
 class ResourcesController extends AbstractController
 {
-    #[Route('/resources', name: 'app_resources')]
+    private ResourcesRepository $resourcesRepository;
+    public function __construct(ResourcesRepository $resourcesRepository)
+    {
+        $this->resourcesRepository = $resourcesRepository;
+    }
+
     public function index(): Response
     {
         return $this->render('resources/index.html.twig', [
             'controller_name' => 'ResourcesController',
+        ]);
+    }
+    #[Route('/')]
+    public function allResources(){
+        $resources = $this->resourcesRepository->findAll();
+        return $this->render('resources/index.html.twig', [
+            'resources' => $resources
         ]);
     }
 }
