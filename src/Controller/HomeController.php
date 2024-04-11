1843385081d9bae4a8e9460bea99d92864e9eab6
AB2326 <adam.boussatha@viacesi.fr>
2024-04-11 10:54:14 +0200

    Skeleton

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    #[Route('/')]
+    public function index(){
         return $this->render('home/index.html.twig');
     }
 }
