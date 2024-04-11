fc26d8fbbf1f3eb0b22a35f8b6cecaad4b22ce51
AB2326 <adam.boussatha@viacesi.fr>
2024-04-11 13:23:28 +0200

    Skeleton

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ResourcesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private ResourcesRepository $resourcesRepository;
+
+    public function __construct(ResourcesRepository $resourcesRepository)
+    {
+        $this->resourcesRepository = $resourcesRepository;
+    }
     #[Route('/')]
     public function index(){
-        return $this->render('home/index.html.twig');
+        $resources = $this->resourcesRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'resources' => $resources
+        ]);
     }
+
 }
