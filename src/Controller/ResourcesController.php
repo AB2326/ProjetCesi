d7fb3e93d1213dcf5931ad305a2ee9f1d6984094
AB2326 <adam.boussatha@viacesi.fr>
2024-04-12 15:28:35 +0200

    Skeleton

--- src/Controller/ResourcesController.php
+++ src/Controller/ResourcesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ResourcesType;
 use App\Repository\ResourcesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,15 +10,36 @@ use Symfony\Component\Routing\Attribute\Route;
 class ResourcesController extends AbstractController
 {
     private ResourcesRepository $resourcesRepository;
+
     public function __construct(ResourcesRepository $resourcesRepository)
     {
         $this->resourcesRepository = $resourcesRepository;
     }
+
     #[Route('/')]
-    public function allResources(){
+    public function allResources()
+    {
         $resources = $this->resourcesRepository->findAll();
         return $this->render('resources/index.html.twig', [
             'resources' => $resources
+        ]);
+    }
+    #[Route('/createArticle', name: 'createArticle')]
+    public function createArticle(Request $request): Response
+    {
+        $article = new Resources();
+        $form = $this->createForm(ResourcesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('modal/publish.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
