7e1ca03c85b6b92441707c2912cd41274d7b96c9
Adrien Vercoutter <adrien.vercoutter@gmail.com>
2024-05-14 10:01:08 +0200

    help login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,12 +12,27 @@ use App\Entity\User;
 use App\Form\LoginType;
 use DateTime;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 
 class LoginController extends AbstractController
 {
+
+    private $entityManager;
+    private $tokenStorage;
+    private $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage, UserPasswordEncoderInterface  $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->tokenStorage = $tokenStorage;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, EntityManagerInterface $em)
     {
@@ -56,30 +71,34 @@ class LoginController extends AbstractController
     }
 
     #[Route('/login', name: 'app_login')]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
+        $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        $form = $this->createForm(LoginType::class, [
-            'email' => $lastUsername,
-        ]);
-
+        $form = $this->createForm(LoginType::class, ['email' => $lastUsername]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $formData = $form->getData();
-
-            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $formData['email']]);
+            $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $formData['email']]);
 
             if ($user) {
-                if ($passwordEncoder->isPasswordValid($user, $formData['password'])) {
+                // var_dump($user);die();
+                if ($this->passwordEncoder->isPasswordValid($user->getPassword(), $formData['firstPassword'], $user->getSalt())) {
+                    $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+                    $this->tokenStorage->setToken($token);
                     return $this->redirectToRoute('app_home');
+                } else {
+                    $this->addFlash('error', 'Mot de passe incorrect.');
                 }
+            } else {
+                $this->addFlash('error', 'Adresse email invalide.');
             }
-
-            $this->addFlash('error', 'Invalid credentials.');
         }
 
         return $this->render('login/index.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -243,4 +243,5 @@ class User
 
         return $this;
     }
+
 }
