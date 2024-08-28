b81b14eaf8a434b728168020950c7f001e7e02f0
AB2326 <adam.boussatha@viacesi.fr>
2024-08-28 13:03:51 +0200

    mise en dev

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -17,7 +17,6 @@ class CommentType extends AbstractType
             ])
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
