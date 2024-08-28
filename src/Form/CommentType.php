69466603bc9ab844a27f21034c58c7f9b6e92629
AB2326 <adam.boussatha@viacesi.fr>
2024-08-28 12:54:23 +0200

    mise en dev

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -5,7 +5,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class CommentType extends AbstractType
 {    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
